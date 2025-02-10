f9f33287a849e0a2fbb13da6721691af024b5fc5
Thiago <thiagocrrabelo@gmail.com>
2025-02-10 02:40:36 -0300

    Making sure interruptions don't interfere with i2c functions

--- i2c.c
+++ i2c.c
@@ -16,19 +16,20 @@ void init_i2c(void) {
 }
 
 uint8_t i2c_test(uint8_t slave_addr) {
+    __disable_interrupt();
     UCB0I2CSA = slave_addr;
     UCB0CTL1 |= UCTR;
     UCB0CTL1 |= UCTXSTT;
     while ((UCB0IFG & UCTXIFG) == 0);
     UCB0CTL1 |= UCTXSTP;
     while ((UCB0IFG & UCTXSTP) == UCTXSTP);
-    if ((UCB0IFG & UCNACKIFG) == 0)
-        return 1;
-    else
-        return 0;
+    const int result = (UCB0IFG & UCNACKIFG) == 0;
+    __enable_interrupt();
+    return result;
 }
 
 void i2c_send(uint8_t slave_addr, uint8_t register_addr, uint8_t data) {
+    __disable_interrupt();
     // Aguarda qualquer transmissão em andamento finalizar
     while (UCB0CTL1 & UCTXSTP);
 
@@ -54,9 +55,12 @@ void i2c_send(uint8_t slave_addr, uint8_t register_addr, uint8_t data) {
 
     UCB0CTL1 |= UCTXSTP; //Gerar STOP
     while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
+
+    __enable_interrupt();
 }
 
 void i2c_read(uint8_t slave_addr, uint8_t register_addr, uint8_t* byte) {
+    __disable_interrupt();
     // Aguarda qualquer transmissão em andamento finalizar
     while (UCB0CTL1 & UCTXSTP);
 
@@ -86,9 +90,11 @@ void i2c_read(uint8_t slave_addr, uint8_t register_addr, uint8_t* byte) {
     while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
 
     *byte = UCB0RXBUF;
+    __enable_interrupt();
 }
 
 void i2c_read_vect(uint8_t slave_addr, uint8_t register_addr, uint8_t byte_count, volatile uint8_t* data) {
+    __disable_interrupt();
     // Aguarda qualquer transmissão em andamento finalizar
     while (UCB0CTL1 & UCTXSTP);
 
@@ -128,4 +134,6 @@ void i2c_read_vect(uint8_t slave_addr, uint8_t register_addr, uint8_t byte_count
      data[i]=UCB0RXBUF; //Ler dado
 
      while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
+
+     __enable_interrupt();
 }
